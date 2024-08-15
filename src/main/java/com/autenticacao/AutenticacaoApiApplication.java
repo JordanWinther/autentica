9747907c1bc26ba02cca40269ab30fce9a34b126
jonathanwinther7 <jonathanwinther7@outlook.com>
2024-08-14 22:35:05 -0300

    Primeiro commit

---
+++ src/main/java/com/autenticacao/AutenticacaoApiApplication.java
@@ -0,0 +1,13 @@
+package com.autenticacao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutenticacaoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutenticacaoApiApplication.class, args);
+	}
+
+}
